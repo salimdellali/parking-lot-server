e143fa68069f52a7910144a3393fb83c44e9419b
Salim DELLALI <dellali.salim@gmail.com>
2021-06-29 17:39:50 +0100

    updated README, an also used the git add --update to delete untracked files

--- dist/helpers/times.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.getCurrentUnixTimeStampInSeconds = () => {
-    return Math.round(new Date().getTime() / 1000);
-};
-exports.diffTimeStampsInSeconds = (timeStampInSeconds1, timeStampInSeconds2) => {
-    return Math.abs(timeStampInSeconds1 - timeStampInSeconds2);
-};
